65fbe4cd4d0d6e4a2716cdd271e30ebaca2f0ed9
Rodrigo Camio <rodricamio94@gmail.com>
2023-10-19 20:36:46 -0300

    Hicimos modificaciones en variables y outputs, tambien creamos un modulo de vpc

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,17 @@ output "instance_id" {
   value       = aws_instance.web.id
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.web.public_ip
+# }
+
+output "vpc_public_subnets" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "ec2_instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instances[*].public_ip
 }
