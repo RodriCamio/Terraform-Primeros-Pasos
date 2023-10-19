7ab8434406d21bb3b6e50e6242a1e6d3b0c50ac1
Rodrigo Camio <rodricamio94@gmail.com>
2023-10-19 19:30:20 -0300

    Primeras modificaciones de terraform

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
 terraform {
-  required_providers {          #requiere de un proveedor 
-    aws = {                     #en este caso AWS
-      source  = "hashicorp/aws" #¿Dónde encuentro el proveedor? En el repositorio de hashicorp
-      version = "~> 3.0"        #queremos instalar una versión superior o igual a 3.0
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
+
 provider "aws" {
-    region = "us-east-1" #configuramos la región de nuestro proveedor
-    access_key = var.access_key #acá usamos la variable que creamos con el "var." llamamos a la variable.
-    secret_key = var.secret_key 
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
 }
